969f4f90c147797eb4269a3e6315236610d73537
bravovaldes <prauvaldes@gmail.com>
2025-08-12 19:49:31 -0400

    service centre

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ from fastapi.staticfiles import StaticFiles
 # 🟢 Import des routers
 from app.api.routes_feedback import router as feedback_router
 from app.api.expression_routes import router as expression_router
-
+from app.api.centres_routes import router as centres_router
 app = FastAPI(title="TCF Express API")
 
 # 🟩 Router Feedback
 app.include_router(feedback_router, prefix="/feedback")
 
-
+app.include_router(centres_router)
 # 🟦 Router Expression (ex. expression écrite/orale)
 app.include_router(expression_router)
